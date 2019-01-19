ef3df782161e2b5350d9ddd1b2f848e324438444
Alexey <none@none>
2019-01-19 13:48:52 +0700

    http://redmine.cyberly.ru/issues/1670
    Версия PHPMailer заменена на более раннюю, так как последняя вызывает ошибки на живом сайте

--- www/vendor/composer/autoload_static.php
+++ www/vendor/composer/autoload_static.php
@@ -6,25 +6,21 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitba2a7e20256cde54197a63ed70dd9cf4
 {
-    public static $prefixLengthsPsr4 = array (
-        'P' => 
-        array (
-            'PHPMailer\\PHPMailer\\' => 20,
-        ),
-    );
-
-    public static $prefixDirsPsr4 = array (
-        'PHPMailer\\PHPMailer\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
-        ),
+    public static $classMap = array (
+        'EasyPeasyICS' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/EasyPeasyICS.php',
+        'PHPMailer' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+        'PHPMailerOAuth' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauth.php',
+        'PHPMailerOAuthGoogle' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauthgoogle.php',
+        'POP3' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.pop3.php',
+        'SMTP' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.smtp.php',
+        'ntlm_sasl_client_class' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/ntlm_sasl_client.php',
+        'phpmailerException' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInitba2a7e20256cde54197a63ed70dd9cf4::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInitba2a7e20256cde54197a63ed70dd9cf4::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInitba2a7e20256cde54197a63ed70dd9cf4::$classMap;
 
         }, null, ClassLoader::class);
     }
